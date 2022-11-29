d957a94e701093fc83a06afaaedb2c40b4989bad
AktureyevOrazali <220103173@stu.sdu.edu.kz>
2022-11-29 16:13:33 +0600

    Solution

--- Questions.java
+++ Questions.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: Orazali");
-        System.out.println("Surname: Aktureyev");
-        System.out.println("ID: 220103173");
-        System.out.println("Hobby: DOTA2, Movies");
-        System.out.println("How much do you like ICT(from 1 - 10): infinity");
-        System.out.println("Rate this task from 1 to 10: 3 points");
-        System.out.println("Am I thw best teacher?: off course, go dota 1x1");
+        System.out.println("Name: Alikhan");
+        System.out.println("Surname: Toleberdyyev");
+        System.out.println("ID: 220103050");
+        System.out.println("Hobby: Coding");
+        System.out.println("How much do you like ICT(from 1 - 10): 15");
+        System.out.println("Rate this task from 1 to 10: 0");
+        System.out.println("Am I thw best teacher?: maybe");
     }
 }
